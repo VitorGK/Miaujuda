d6183e4235d7013d6a253086a871737a8a866cf7
VitorGK <vitorgk1@gmail.com>
2021-11-26 08:42:32 -0300

    Initial Commit

---
+++ Project P/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project P
+//
+//  Created by Vitor Grechi Kuninari on 26/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project P/Project_PApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_PApp.swift
+//  Project P
+//
+//  Created by Vitor Grechi Kuninari on 26/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
