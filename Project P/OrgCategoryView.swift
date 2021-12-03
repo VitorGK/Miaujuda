f9bcaec8c17db1960e03e5c260b6ac267ad2f426
caroltaus <carolinetaus@hotmail.com>
2021-12-03 16:22:27 -0300

    tela de categoria da org

--- Project P/OrgCategoryView.swift
+++ Project P/OrgCategoryView.swift
@@ -6,12 +6,44 @@
 //
 
 import SwiftUI
-
 struct OrgCategoryView: View {
+
+    let categories = ["ONG", "Protetor Independente", "Loja", "Fornecedor"]
+    @State var selectedCategory: String? = nil
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(categories, id: \.self) { item in
+                OrgSelectionCell(category: item, selectedCategory: self.$selectedCategory)
+            }
+        }
+        .navigationTitle("Categoria")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
+
+struct OrgSelectionCell: View {
+
+    let category: String
+    @Binding var selectedCategory: String?
+
+    var body: some View {
+        HStack {
+            Text(category)
+            Spacer()
+            if category == selectedCategory {
+                Image(systemName: "checkmark")
+                    .foregroundColor(.accentColor)
+            }
+        }
+        .background(Color.white)
+        .onTapGesture {
+                self.selectedCategory = self.category
+            }
+    }
+}
+
+
 
 struct OrgCategoryView_Previews: PreviewProvider {
     static var previews: some View {
