656d0c54f47478435e60529a5d89d72537bf3d87
caroltaus <carolinetaus@hotmail.com>
2021-12-08 15:03:35 -0300

    Pagina de category search

--- Project P/CategorySearch.swift
+++ Project P/CategorySearch.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct CategorySearch: View {
     @Binding var category: String
+    let columns = [
+        GridItem(.adaptive(minimum: 180))
+    ]
+    
+    var posts: [PetPost] = [
+        PetPost(_id: "", createdAt: Date(), userID: "", status: "", type: "donation", title: "toti", description: "ljksdfklsd"),
+        PetPost(_id: "", createdAt: Date(), userID: "", status: "", type: "necessity", title: "gfdgdf", description: "ljksdfklsd")
+            ]
     
   //TODO: Exibir os posts filtrados para cada categoria
     var body: some View {
         ScrollView {
-            VStack {
-                PostCard(title: "titulo", organization: "org", items: [PostItem(name: "nomeitem", quantity: "qtditem")], status: "active", timeStamp: Date(), type: "donation", localization: "local")
-            } .padding()
+            LazyVGrid(columns: columns, spacing: 10) {
+
+                ForEach(0..<posts.filter {$0.category == "\(category)"}.count, id: \.self) { p in
+
+                    NavigationLink {
+                        PostDetailsView(post: posts.filter {$0.category == "\(category)"}[p])
+                    } label: { //TODO: os atributos organizaçao, localizacao são do usuario
+                        PostCard(title: posts.filter {$0.category == "\(category)"}[p].title, organization: posts.filter {$0.category == "\(category)"}[p].userID, items: [PostItem(name: "item", quantity: "2 sacos")], status: posts.filter {$0.category == "\(category)"}[p].status, timeStamp: posts.filter {$0.category == "\(category)"}[p].createdAt, type: posts.filter {$0.category == "\(category)"}[p].type, localization: posts.filter {$0.category == "\(category)"}[p].userID)
+
+                    }.buttonStyle(.plain)
+
+                }
+            
+            
+            
         } .navigationTitle("\(category)")
     }
 }
