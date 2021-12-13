df9db2c7134b2441b70d4ce46ccc180dc7f77198
João Victor Ferreira Pimenta <joaovictorfp30@gmail.com>
2021-12-13 15:10:14 -0300

    iniciando o AppClips

---
+++ HelpetsAC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelpetsAC
+//
+//  Created by João Victor Ferreira Pimenta on 13/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
