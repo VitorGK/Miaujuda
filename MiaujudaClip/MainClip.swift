90481717be8c4cba437c3183b0688ab3728bf647
Bianca Maciel <macielmatosb@gmail.com>
2021-12-15 12:23:13 -0300

    Criando AppClip

---
+++ MiaujudaClip/MainClip.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+
+struct MainClip: View {
+    @AppStorage("avatar") var avatar: Int = 0
+    @AppStorage("userID") var userID: String = ""
+    
+    @ObservedObject var postViewModel: PostViewModel
+    
+
+    @State var pickerSelectedItem: Int = 1
+
+    let columns = [
+        GridItem(.adaptive(minimum: 180))
+    ]
+    
+    init() {
+        postViewModel = PostViewModel()
+    }
+    
+    @ViewBuilder
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical) {
+                
+                VStack {
+                    Picker(selection: $pickerSelectedItem, label: Text("Picker"), content: {
+                        Text("Necessidades").tag(1)
+                        Text("Doações").tag(2)
+                    })
+                        .pickerStyle(SegmentedPickerStyle())
+                    
+                    if pickerSelectedItem == 1 {
+                        PostsGrid(posts: filterPosts(type: "Necessidade", posts: postViewModel.posts))
+                    } else {
+                        PostsGrid(posts: filterPosts(type: "Doação", posts: postViewModel.posts))
+                    }
+                }
+                .padding(.leading)
+                .padding(.trailing)
+                
+                Spacer()
+            }
+            .navigationTitle("Postagens")
+            // TODO: Add profile button
+            .navigationBarTitleDisplayMode(.large)
+        }
+    }
+    
+    func filterPosts(type: String, posts: [PetPost]) -> [PetPost] {
+        var filteredPosts: [PetPost]
+        filteredPosts = posts.filter {$0.type == "\(type)"}
+        return filteredPosts
+    }
+}
+
+struct MainClip_Previews: PreviewProvider {
+    static var previews: some View {
+        MainClip()
+    }
+}
