0f12972b6d7b28a75134609bfd716fc76ae59e4c
VitorGK <vitorgk1@gmail.com>
2021-12-16 12:25:30 -0300

    Update PostDetailsView.swift

--- Miaujuda/Post/PostDetailsView.swift
+++ Miaujuda/Post/PostDetailsView.swift
@@ -1,12 +1,9 @@
 import SwiftUI
 
 struct PostDetailsView: View {
-    var post: PetPost
-    @ObservedObject var userViewModel = UserInfos()
+    var petPost: PetPost
     
-    init(post: PetPost) {
-        self.post = post
-    }
+    @State var user: User?
     
     let avatarImages: [String] = [
         "profileCat1",
@@ -15,17 +12,15 @@ struct PostDetailsView: View {
         "profileCat2"
     ]
     
-    
     var body: some View {
-        
         ScrollView(.vertical) {
             VStack(alignment:.leading){
-                Text(String(post.createdAt))
+                Text(self.petPost.createdAt)
                     .font(.subheadline)
                     .foregroundColor(Color.secondary)
                     .padding(.bottom)
                 
-                Text(post.description)
+                Text(self.petPost.description)
                     .padding(.bottom)
                 
                 HStack{
@@ -33,7 +28,7 @@ struct PostDetailsView: View {
                         .font(.title2.bold())
                         .padding(.trailing)
                     
-                    Text(post.type)
+                    Text(self.petPost.type)
                         .font(.title3)
                 }
                 
@@ -41,64 +36,78 @@ struct PostDetailsView: View {
                     .font(.title2.bold())
                     .padding(.top)
                 
-                ItemCard(itemName: post.itemName, itemQuantity: post.itemQuantity, itemCategory: post.itemCategory, itemExpirationDate: post.itemExpirationDate)
+                ItemCard(itemName: self.petPost.itemName, itemQuantity: self.petPost.itemQuantity, itemCategory: self.petPost.itemCategory, itemExpirationDate: self.petPost.itemExpirationDate)
                 
                 Text("Contatos")
                     .font(.title2.bold())
                     .padding(.top)
                 
                 HStack{
-                    if let img = userViewModel.user?.avatar {
+                    if let img = self.user?.avatar {
                         Image(avatarImages[img])
                             .resizable()
                             .frame(width: 60, height: 60)
-                    }
-                    else {
+                    } else {
                         RoundedRectangle(cornerRadius: 30)
                             .fill(.black)
                             .frame(width: 60, height: 60)
                     }
                     
-                    
-                    
                     VStack(alignment: .leading){
-                        Text(userViewModel.user?.organizationName ?? "")
-                        Text(userViewModel.user?.organizationCategory ?? "")
-                        Text(userViewModel.user?.organizationZipCode ?? "")
+                        Text(self.user?.organizationName ?? "")
+                        Text(self.user?.organizationCategory ?? "")
+                        Text(self.user?.organizationZipCode ?? "")
                     }
                 }
                 .padding(.bottom)
                 
-                                VStack (alignment: .leading){
-                                    if let email = userViewModel.user?.email, !email.isEmpty {
-                                        Label("\(email)", systemImage: "envelope.fill")
-                                    }
-                                    if let phone = userViewModel.user?.phone, !phone.isEmpty {
-                                        Label("\(phone)", systemImage: "phone.fill")
-                                    }
-                                    if let website = userViewModel.user?.website, !website.isEmpty {
-                                        Label("\(website)", systemImage: "link")
-                                    }
-                                }
+                VStack (alignment: .leading){
+                    if let email = self.user?.email ?? "", !email.isEmpty {
+                        Label("\(email)", systemImage: "envelope.fill")
+                    }
+                    if let phone = self.user?.phone ?? "", !phone.isEmpty {
+                        Label("\(phone)", systemImage: "phone.fill")
+                    }
+                    if let website = self.user?.website ?? "", !website.isEmpty {
+                        Label("\(website)", systemImage: "link")
+                    }
+                }
             }
             .padding()
         }
-        .navigationTitle(post.title)
+        .navigationTitle(self.petPost.title)
         .navigationBarTitleDisplayMode(.large)
-        .navigationBarItems(trailing: editMenu())
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                EditMenu()
+            }
+        }
         .onAppear {
-            userViewModel.getUserFromPost(post: post)
+            ServerService.shared.getUser(by: self.petPost.userID) { result in
+                DispatchQueue.main.async {
+                    switch result{
+                        case .success(let user):
+                            self.user = user
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                    }
+                }
+            }
         }
     }
 }
 
-struct editMenu: View {
+struct EditMenu: View {
     var body: some View {
         Menu {
-            Button(action: concluir) {
+            Button {
+                concluir()
+            } label: {
                 Label("Concluir post", systemImage: "checkmark")
             }
-            Button(role: .destructive, action: deletar) {
+            Button(role: .destructive) {
+                deletar()
+            } label: {
                 Label("Deletar post", systemImage: "trash.fill")
             }
         } label: {
@@ -106,11 +115,12 @@ struct editMenu: View {
         }
     }
     
-    func concluir(){
+    func concluir() {
         print("post concluido")
+        // TODO: Add PUT request
     }
     
-    func deletar(){
+    func deletar() {
         print("deletado")
     }
 }
