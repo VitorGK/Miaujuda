05cbee433f92530409653c13adf328c6be8124a1
VitorGK <vitorgk1@gmail.com>
2021-12-16 12:23:58 -0300

    Remove commented code

--- Miaujuda/Utils/API/ServerService.swift
+++ Miaujuda/Utils/API/ServerService.swift
@@ -6,7 +6,7 @@ class ServerService {
     private let baseUrl: String = "https://project-pets.herokuapp.com"
     private let jwtToken = UserDefaults.standard.string(forKey: "jwtToken")
     
-    // MARK: --- GET REQUEST
+    // MARK: --- Get request
     func getRequest(route: UrlRoute, id: String = "", completion: @escaping (Result<[String: Any], Error>) -> Void) {
         let session = URLSession.shared
         guard let url = URL(string: baseUrl + route.rawValue + (id != "" ? "/" + id : "")) else { return }
@@ -27,7 +27,7 @@ class ServerService {
         .resume()
     }
     
-    // MARK: --- GET ALL POSTS
+    // MARK: --- Get all PetPosts
     func getAllPetPosts(completion: @escaping (Result<[PetPost], Error>) -> Void) {
         let session = URLSession.shared
         guard let url = URL(string: baseUrl + UrlRoute.petPost.rawValue) else { return }
@@ -46,7 +46,7 @@ class ServerService {
         .resume()
     }
     
-    // MARK: --- Get Latest PetPost and User data
+    // MARK: --- Get latest PetPost and User data
     func getLatestPetPost(completion: @escaping (Result<PetPostAndUser, Error>) -> Void) {
         let session = URLSession.shared
         guard let url = URL(string: baseUrl + UrlRoute.latestPetPost.rawValue) else { return }
@@ -65,45 +65,29 @@ class ServerService {
         .resume()
     }
     
-    // MARK: --- GET USER BY ID
+    // MARK: --- Get User by ID
         func getUser(by id: String, completion: @escaping (Result<User, Error>) -> Void) {
             let session = URLSession.shared
-            guard let url = URL(string: baseUrl + UrlRoute.user.rawValue + id) else {
-//                assertionFailure()
-//                completion(.failure(NSError(domain: "Invalid URL", code: Int(errSecParam))))
-                return
-            }
+            guard let url = URL(string: baseUrl + UrlRoute.user.rawValue + id) else { return }
             var request = URLRequest(url: url)
-            guard let jwtToken = self.jwtToken else {
-                //assertionFailure()
-//                completion(.failure(NSError(domain: "Invalid JWT", code: Int(errSecAuthFailed))))
-                return
-            }
+//            guard let jwtToken = self.jwtToken else { return }
+//            request.setValue("Bearer \(jwtToken)", forHTTPHeaderField: "Authorization")
             
-            request.setValue("Bearer \(jwtToken)", forHTTPHeaderField: "Authorization")
-
             session.dataTask(with: request) { data, _, error in
                 if let data = data {
                     do {
-//                        let JSONString = String(data: data, encoding: .utf8)
-//                        print(JSONString)
                         let responseJson = try JSONDecoder().decode(User.self, from: data)
                         completion(.success(responseJson))
                     } catch {
                         print(error)
                         completion(.failure(error))
                     }
-                } /*else if let error = error {
-                    completion(.failure(error))
-                } else {
-                    assertionFailure()
-                    completion(.failure(NSError(domain: "Unexpected code", code: Int(errSecInvalidScope))))
-                }*/
+                }
             }
             .resume()
         }
     
-    // MARK: --- POST REQUEST
+    // MARK: --- Post request
     public func postRequest(route: UrlRoute, body: [String: Any], completion: @escaping (Result<[String: Any], Error>) -> Void) {
         let session = URLSession.shared
         guard let url = URL(string: baseUrl + route.rawValue) else { return }
@@ -117,8 +101,8 @@ class ServerService {
         session.dataTask(with: request) { data, _, error in
             if let data = data {
                 do {
-                    guard let responseJson = try JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
-                    completion(.success(responseJson))
+                    guard let data = try JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+                    completion(.success(data))
                 } catch let error {
                     completion(.failure(error))
                 }
@@ -127,13 +111,12 @@ class ServerService {
         .resume()
     }
 
-    // MARK: --- AUTH USER (GET JWT TOKEN)
+    // MARK: --- Authenticate User and Get JWT token
     func authenticate(appleID: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
         let session = URLSession.shared
         guard let url = URL(string: baseUrl + UrlRoute.auth.rawValue) else { return }
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        
         let appleIdData = ["appleID": appleID]
         guard let httpBody = try? JSONEncoder().encode(appleIdData) else { return }
         request.httpBody = httpBody
