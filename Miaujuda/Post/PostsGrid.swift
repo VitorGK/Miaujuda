4897862f0dfff0e05afbe8984d9b0c8fdd109b68
VitorGK <vitorgk1@gmail.com>
2021-12-16 12:25:21 -0300

    Update PostsGrid.swift

--- Miaujuda/Post/PostsGrid.swift
+++ Miaujuda/Post/PostsGrid.swift
@@ -1,26 +1,21 @@
-//
-//  PostsGrid.swift
-//  Helpets
-//
-//  Created by Caroline Taus on 09/12/21.
-//
-
 import SwiftUI
 
 struct PostsGrid: View {
     @State var posts: [PetPost]
+    
     let columns = [
-            GridItem(.adaptive(minimum: 180))
-        ]
+        GridItem(.adaptive(minimum: 180))
+    ]
+    
     var body: some View {
         LazyVGrid(columns: columns, spacing: 10) {
             ForEach(0..<posts.count, id: \.self) { p in
                 NavigationLink {
-                    PostDetailsView(post: posts[p])
+                    PostDetailsView(petPost: posts[p])
                 } label: {
-                    PostCard(post: posts[p])
-                }.buttonStyle(.plain)
-
+                    PostCard(petPost: posts[p])
+                }
+                .buttonStyle(.plain)
             }
         }
     }
